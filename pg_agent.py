4cb18cd798afb0a779ac4269e865f07d6755f831
Sergey Kishchenko <voidwrk@gmail.com>
2016-07-01 12:36:51 -0700

    PGAgent added

--- pg_agent.py
+++ pg_agent.py
@@ -13,25 +13,10 @@ import csv
 import datetime as dt
 import sys
 
-seed = 1
-random.seed(seed)
-np.random.seed(seed)
-tf.set_random_seed(seed)
-
-dtype = tf.float32
-
-
-
-# hyperparameters
-#H = 83 # number of hidden layer neurons
-# learning_rate = 1e-3
-GAMMA = 0.99 # discount factor for reward
-# decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
-render = False
-monitor = False
-# epochs = 15
-
-
+DTYPE = tf.float32
+RENDER_EVERY = 100
+MONITOR = False
+# Sample from probability distribution.
 def cat_sample(prob_nk):
     assert prob_nk.ndim == 2
     N = prob_nk.shape[0]
@@ -44,12 +29,7 @@ def cat_sample(prob_nk):
                 break
     return out
 
-def softmax(w, t = 1.0):
-    e = np.exp(w / t)
-    dist = e / np.sum(e)
-    return dist
-
-def discount_rewards(r, gamma = GAMMA):
+def discount_rewards(r, gamma):
   """ take 1D float array of rewards and compute discounted reward """
   discounted_r = np.zeros_like(r)
   running_add = 0
@@ -59,9 +39,9 @@ def discount_rewards(r, gamma = GAMMA):
     discounted_r[t] = running_add
   return discounted_r
 
-
-class LearningAgent(object):
-  def __init__(self, env, H, timesteps_per_batch, learning_rate, gamma, epochs, dropout, win_reward):
+# Learning agent. Incapsulates training and prediction.
+class PGAgent(object):
+  def __init__(self, env, win_step, H, timesteps_per_batch, learning_rate, gamma, epochs, dropout, win_reward):
     if not isinstance(env.observation_space, Box) or \
        not isinstance(env.action_space, Discrete):
         print("Incompatible spaces.")
@@ -74,28 +54,36 @@ class LearningAgent(object):
     self.epochs = epochs
     self.dropout = dropout
     self.win_reward = win_reward
-
+    self.win_step = win_step
     self.env = env
     self.session = tf.Session()
+
+    # Full state used for next action prediction. Contains current
+    # observation, previous observation and previous action.
     self.obs = tf.placeholder(
-        dtype, shape=[
+        DTYPE, shape=[
             None, 2 * env.observation_space.shape[0] + env.action_space.n], name="obs")
     self.prev_obs = np.zeros(env.observation_space.shape[0])
     self.prev_action = np.zeros(env.action_space.n)
-    self.action = action = tf.placeholder(dtype, shape=[None, env.action_space.n], name="action")
-    self.advant = advant = tf.placeholder(dtype, shape=[None], name="advant")
-    self.prev_policy = action = tf.placeholder(dtype, shape=[None, env.action_space.n], name="prev_policy")
+
+    # One hot encoding of the actual action taken.
+    self.action = action = tf.placeholder(DTYPE, shape=[None, env.action_space.n], name="action")
+    # Advantage, obviously.
+    self.advant = advant = tf.placeholder(DTYPE, shape=[None], name="advant")
+    # Old policy prediction.
+    self.prev_policy = action = tf.placeholder(DTYPE, shape=[None, env.action_space.n], name="prev_policy")
 
     self.policy_network, _ = (
         pt.wrap(self.obs)
             .fully_connected(H, activation_fn=tf.nn.tanh)
             .dropout(self.dropout)
             .softmax_classifier(env.action_space.n))
-    self.returns = tf.placeholder(dtype, shape=[None, env.action_space.n], name="returns")
+    self.returns = tf.placeholder(DTYPE, shape=[None, env.action_space.n], name="returns")
 
-    loss = - tf.reduce_sum(tf.mul(self.action, tf.div(self.policy_network, self.prev_policy)), 1) * self.advant
+    loss = - tf.reduce_sum(tf.mul(self.action,
+                                  tf.div(self.policy_network,
+                                         self.prev_policy)), 1) * self.advant
     self.train = tf.train.AdamOptimizer().minimize(loss)
-
     self.session.run(tf.initialize_all_variables())
 
   def rollout(self, max_pathlength, timesteps_per_batch):
@@ -108,9 +96,9 @@ class LearningAgent(object):
       self.prev_action *= 0.0
       self.prev_obs *= 0.0
       for x in xrange(max_pathlength):
-        if render and 0==x % 20: env.render()
-        # import pdb; pdb.set_trace()
-        obs_new = np.expand_dims(np.concatenate([ob, self.prev_obs, self.prev_action], 0), 0)
+        if not RENDER_EVERY is None and 0==x % RENDER_EVERY: env.render()
+        obs_new = np.expand_dims(
+            np.concatenate([ob, self.prev_obs, self.prev_action], 0), 0)
 
         action_dist_n = self.session.run(self.policy_network, {self.obs: obs_new})
 
@@ -125,7 +113,7 @@ class LearningAgent(object):
         actions_one_hot.append(np.copy(self.prev_action))
 
         res = list(self.env.step(action))
-        if 199==len(rewards):
+        if not self.win_step is None and self.win_step==len(rewards):
           rewards.append(self.win_reward)
           res[2] = True
         else:
@@ -148,7 +136,6 @@ class LearningAgent(object):
     return paths
 
   def prepare_features(self, path):
-    # import pdb; pdb.set_trace()
     obs = path["obs"]
     prev_obs = np.concatenate([np.zeros((1,obs.shape[1])), path["obs"][1:]], 0)
     prev_action = path['action_dists']
@@ -160,7 +147,6 @@ class LearningAgent(object):
 
   def learn(self):
     self.current_observation = env.reset()
-#    self.prev_x = None # used in computing the difference frame
 
     xs,hs,dlogps,drs = [],[],[],[]
 
@@ -172,13 +158,12 @@ class LearningAgent(object):
     discounted_eprs = []
     mean_path_lens = []
     while True:
-
-      paths = self.rollout(max_pathlength=10000, timesteps_per_batch=self.timesteps_per_batch)
+      paths = self.rollout(max_pathlength=10000,
+                           timesteps_per_batch=self.timesteps_per_batch)
 
       for path in paths:
         path["prev_policy"] = self.predict_for_path(path)
         path["returns"] = discount_rewards(path["rewards"], self.gamma)
-        # path["advant"] = path["returns"] - path["baseline"]
         path["advant"] = path["returns"]
 
       features = np.concatenate([self.prepare_features(path) for path in paths])
@@ -190,7 +175,6 @@ class LearningAgent(object):
       actions = np.concatenate([path["actions_one_hot"] for path in paths])
       prev_policy = np.concatenate([path["prev_policy"] for path in paths])
 
-      # predictions = np.concatenate([self.predict_for_path(path) for path in paths])
       for _ in range(self.epochs):
         self.session.run(self.train, {self.obs: features,
                                       self.advant: advant,
@@ -200,46 +184,32 @@ class LearningAgent(object):
 
       mean_path_len = np.mean([len(path['rewards']) for path in paths])
       mean_path_lens.append(mean_path_len)
+      print "Current iteration mean_path_len: %s" % mean_path_len
       if iteration_number > 100:
         paths = self.rollout(max_pathlength=10000, timesteps_per_batch=40000)
         return np.mean([len(path['rewards']) for path in paths]), np.mean(mean_path_lens)
-      # if 0 == iteration_number % 25:
-      #   print "Iteration %s finished. Mean path len: %s" % (iteration_number, mean_path_lens[-10:])
 
-env = gym.make("CartPole-v0")
-training_dir = tempfile.mkdtemp()
+if __name__ == '__main__':
+  seed = 1
+  random.seed(seed)
+  np.random.seed(seed)
+  tf.set_random_seed(seed)
 
-# f = open('/Users/tilarids/Downloads/study_599437103_trials.csv')
-# reader = csv.DictReader(f)
-# for experiment in reader:
-#   if experiment['Status'] != 'PENDING':
-#     continue
-#   agent = LearningAgent(env,
-#                         int(experiment['H']),
-#                         int(experiment['timesteps_per_batch']),
-#                         float(experiment['learning_rate']),
-#                         float(experiment['gamma']),
-#                         int(experiment['epochs']),
-#                         float(experiment['dropout']),
-#                         float(experiment['win_reward']))
-#   time_before = dt.datetime.now()
-#   validation_mean_rewards, train_mean_rewards = agent.learn()
-#   elapsed_secs = (dt.datetime.now() - time_before).seconds
-#   print "For TrialId=%s validation result is %s and train result is %s in %s secs" % (experiment['TrialId'], validation_mean_rewards, train_mean_rewards, elapsed_secs)
+  env = gym.make("CartPole-v0")
+  if MONITOR:
+    training_dir = tempfile.mkdtemp()
+    env.monitor.start(training_dir)
 
-
-env.monitor.start(training_dir)
-
-# H, timesteps_per_batch, learning_rate, gamma, epochs, dropout, win_reward
-agent = LearningAgent(env,
-                      87,
-                      1793,
-                      0.0000022134469959712048,
-                      0.9709822161334865,
-                      52,
-                      0.7600433610799155,
-                      317.75621177926485)
-agent.learn()
-
-env.monitor.close()
-gym.upload(training_dir, api_key='sk_lgS7sCv1Qxq5HFMdQXR6Sw')
+  agent = PGAgent(env,
+                  win_step=None,
+                  H=87,
+                  timesteps_per_batch=1793,
+                  learning_rate=0.0000022134469959712048,
+                  gamma=0.9709822161334865,
+                  epochs=52,
+                  dropout=0.7600433610799155,
+                  win_reward=317.75621177926485)
+  agent.learn()
+  if MONITOR:
+    env.monitor.close()
+    gym.upload(training_dir, api_key='sk_lgS7sCv1Qxq5HFMdQXR6Sw')
