ff29b06690a579dd45970abd35bb21af64771683
Sergey Kishchenko <voidwrk@gmail.com>
2016-07-01 14:13:36 -0700

    ValueFunction added

--- pg_agent.py
+++ pg_agent.py
@@ -14,9 +14,10 @@ import datetime as dt
 import sys
 
 DTYPE = tf.float32
-RENDER_EVERY = 10
+RENDER_EVERY = None
 MONITOR = False
-# Sample from probability distribution.
+
+# Sample from the probability distribution.
 def cat_sample(prob_nk):
     assert prob_nk.ndim == 2
     N = prob_nk.shape[0]
@@ -29,6 +30,13 @@ def cat_sample(prob_nk):
                 break
     return out
 
+def write_csv(file_name, *arrays):
+  import csv
+  with open(file_name, 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for row in zip(*arrays):
+      writer.writerow(row)
+
 def discount_rewards(r, gamma):
   """ take 1D float array of rewards and compute discounted reward """
   discounted_r = np.zeros_like(r)
@@ -39,7 +47,50 @@ def discount_rewards(r, gamma):
     discounted_r[t] = running_add
   return discounted_r
 
-# Learning agent. Incapsulates training and prediction.
+# Separate network to approximate value function. Need to init variables after
+# this is instantiated.
+class ValueFunction(object):
+  def __init__(self, session, features_count, learning_rate, epochs, dropout):
+    self.features_count = features_count
+    self.learning_rate = learning_rate
+    self.epochs = epochs
+    self.session = session
+    self.dropout = dropout
+    self.x = tf.placeholder(tf.float32, shape=[None, features_count], name="x")
+    self.y = tf.placeholder(tf.float32, shape=[None], name="y")
+    self.net = (pt.wrap(self.x)
+                .fully_connected(64, activation_fn=tf.nn.relu)
+                .dropout(self.dropout)
+                .fully_connected(1))
+    self.net = tf.reshape(self.net, (-1, ))
+    self.l2 = (self.net - self.y) * (self.net - self.y)
+    self.train = tf.train.AdamOptimizer(self.learning_rate).minimize(self.l2)
+
+  def prepare_features(self, path):
+    obs = path["obs"]
+    prev_obs = np.concatenate([np.zeros((1, obs.shape[1])), path["obs"][1:]], 0)
+    prev_action = path['action_dists']
+    l = len(path["rewards"])
+    arange = np.arange(l).reshape(-1, 1)
+    ret = np.concatenate([obs, prev_obs, prev_action, arange, np.ones((l, 1))], axis=1)
+    return ret
+
+  def fit(self, paths):
+    features = np.concatenate([self.prepare_features(path) for path in paths])
+    returns = np.concatenate([path["returns"] for path in paths])
+    for _ in range(self.epochs):
+        self.session.run(self.train, {self.x: features, self.y: returns})
+
+  def validate(self, paths):
+    features = np.concatenate([self.prepare_features(path) for path in paths])
+    returns = np.concatenate([path["returns"] for path in paths])
+    return np.mean(self.session.run(self.l2, {self.x: features, self.y: returns}))
+
+  def predict(self, path):
+    features = self.prepare_features(path)
+    return self.session.run(self.net, {self.x: features})
+
+# Learning agent. Encapsulates training and prediction.
 class PGAgent(object):
   def __init__(self, env, win_step, H, timesteps_per_batch, learning_rate, gamma, epochs, dropout, win_reward):
     if not isinstance(env.observation_space, Box) or \
@@ -61,29 +112,43 @@ class PGAgent(object):
     # Full state used for next action prediction. Contains current
     # observation, previous observation and previous action.
     self.obs = tf.placeholder(
-        DTYPE, shape=[
-            None, 2 * env.observation_space.shape[0] + env.action_space.n], name="obs")
+        DTYPE,
+        shape=[None, 2 * env.observation_space.shape[0] + env.action_space.n],
+        name="obs")
     self.prev_obs = np.zeros(env.observation_space.shape[0])
     self.prev_action = np.zeros(env.action_space.n)
 
     # One hot encoding of the actual action taken.
-    self.action = action = tf.placeholder(DTYPE, shape=[None, env.action_space.n], name="action")
+    self.action = tf.placeholder(DTYPE,
+                                 shape=[None, env.action_space.n],
+                                 name="action")
     # Advantage, obviously.
-    self.advant = advant = tf.placeholder(DTYPE, shape=[None], name="advant")
+    self.advant = tf.placeholder(DTYPE, shape=[None], name="advant")
     # Old policy prediction.
-    self.prev_policy = action = tf.placeholder(DTYPE, shape=[None, env.action_space.n], name="prev_policy")
+    self.prev_policy = tf.placeholder(DTYPE,
+                                      shape=[None, env.action_space.n],
+                                      name="prev_policy")
 
     self.policy_network, _ = (
         pt.wrap(self.obs)
             .fully_connected(H, activation_fn=tf.nn.tanh)
             .dropout(self.dropout)
             .softmax_classifier(env.action_space.n))
-    self.returns = tf.placeholder(DTYPE, shape=[None, env.action_space.n], name="returns")
+    self.returns = tf.placeholder(DTYPE,
+                                  shape=[None, env.action_space.n],
+                                  name="returns")
 
     loss = - tf.reduce_sum(tf.mul(self.action,
                                   tf.div(self.policy_network,
                                          self.prev_policy)), 1) * self.advant
     self.train = tf.train.AdamOptimizer().minimize(loss)
+
+    features_count = 2 * env.observation_space.shape[0] + env.action_space.n + 2
+    self.value_function = ValueFunction(self.session,
+                                        features_count,
+                                        learning_rate=1e-3,
+                                        epochs=50,
+                                        dropout=0.5)
     self.session.run(tf.initialize_all_variables())
 
   def rollout(self, max_pathlength, timesteps_per_batch, render=False):
@@ -140,9 +205,9 @@ class PGAgent(object):
     obs = path["obs"]
     prev_obs = np.concatenate([np.zeros((1,obs.shape[1])), path["obs"][1:]], 0)
     prev_action = path['action_dists']
-    return np.concatenate([obs, prev_obs, prev_action], 1)
+    return np.concatenate([obs, prev_obs, prev_action], axis=1)
 
-  def predict_for_path(self, path):
+  def predict(self, path):
     features = self.prepare_features(path)
     return self.session.run(self.policy_network, {self.obs: features})
 
@@ -158,6 +223,8 @@ class PGAgent(object):
     iteration_number = 0
     discounted_eprs = []
     mean_path_lens = []
+    value_function_losses = []
+
     while True:
       render = not RENDER_EVERY is None and 0 == iteration_number % RENDER_EVERY
       paths = self.rollout(max_pathlength=10000,
@@ -165,10 +232,13 @@ class PGAgent(object):
                            render=render)
 
       for path in paths:
-        path["prev_policy"] = self.predict_for_path(path)
+        path["baseline"] = self.value_function.predict(path)
+        path["prev_policy"] = self.predict(path)
         path["returns"] = discount_rewards(path["rewards"], self.gamma)
-        path["advant"] = path["returns"]
+        path["advant"] = path["returns"] - path["baseline"]
 
+      value_function_losses.append(self.value_function.validate(paths))
+      self.value_function.fit(paths)
       features = np.concatenate([self.prepare_features(path) for path in paths])
 
       advant = np.concatenate([path["advant"] for path in paths])
@@ -188,10 +258,12 @@ class PGAgent(object):
       mean_path_len = np.mean([len(path['rewards']) for path in paths])
       mean_path_lens.append(mean_path_len)
       print "Iteration %s mean_path_len: %s" % (iteration_number, mean_path_len)
-      if iteration_number > 200:
+      if iteration_number > 100:
         paths = self.rollout(max_pathlength=10000, timesteps_per_batch=40000)
         ret = np.mean([len(path['rewards']) for path in paths]), np.mean(mean_path_lens)
         print "Validation result: %s" % (ret[0])
+        if not MONITOR:
+          write_csv('/tmp/out.csv', mean_path_lens, value_function_losses)
         return ret
 
 if __name__ == '__main__':
@@ -206,14 +278,14 @@ if __name__ == '__main__':
     env.monitor.start(training_dir)
 
   agent = PGAgent(env,
-                  win_step=None,
+                  win_step=199,
                   H=53,
                   timesteps_per_batch=942,
                   learning_rate=0.0001711781685234833,
                   gamma=0.8676106987337779,
                   epochs=7,
                   dropout=0.6361959729200584,
-                  win_reward=387.48113155097553)
+                  win_reward=1)
   agent.learn()
   if MONITOR:
     env.monitor.close()
